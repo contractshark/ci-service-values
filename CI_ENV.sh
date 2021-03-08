90a9baaf6938507f142000ccaee1147221c5eedb
sam bacha <sam@freighttrust.com>
2021-03-08 00:35:57 -0800

    Create CI_ENV.sh

---
+++ CI_ENV.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+CI_NAME=
+elif [ "$CI" = "true" ] && [ "$CI_NAME" = "codeship" ];
+elif [ "$TEAMCITY_VERSION" != "" ];
+elif [ "$CI" = "true" ] && [ "$CIRCLECI" = "true" ];
+elif [ "$CI" = "drone" ] || [ "$DRONE" = "true" ];
+elif [ "$GITHUB_ACTIONS" != "" ];
