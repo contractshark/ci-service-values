fcb439b0a4eb85b933ec2be305ef5e1b5121ca5d
sam bacha <sam@freighttrust.com>
2021-08-26 00:02:07 -0700

    git clean -ffdx && git reset --hard HEAD
    actions/checkout

--- CI_SERVICE.sh
+++ CI_SERVICE.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+
 /usr/bin/git -c protocol.version=2 fetch --no-tags --prune --progress --no-recurse-submodules --depth=1 origin 
 /usr/bin/git config --local http.https://github.com/.extraheader AUTHORIZATION: basic ***
 /usr/bin/git config --local gc.auto 0
@@ -8,7 +9,9 @@
 /usr/bin/git submodule foreach --recursive git config --local --name-only --get-regexp 'http\.https\:\/\/github\.com\/\.extraheader' && git config --local --unset-all 'http.https://github.com/.extraheader' || :
 /usr/bin/git config --local http.https://github.com/.extraheader AUTHORIZATION: basic ***
 /usr/bin/git -c protocol.version=2 fetch --no-tags --prune --progress --no-recurse-submodules --depth=1 origin 
+/usr/bin/git clean -ffdx && git reset --hard HEAD
 /usr/bin/git checkout --progress --force -B ${GIT_BRANCH_ORIGIN} refs/remotes/origin/${GIT_BRANCH_ORIGIN}
+
 /usr/bin/git log -1 --format='%H'
 /usr/bin/bash -e {0}
 
